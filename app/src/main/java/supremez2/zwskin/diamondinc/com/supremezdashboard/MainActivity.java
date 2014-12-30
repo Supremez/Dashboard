10d30455570ef7c2ab6c9fb159e0a6b52be4fa31
Tim Bremer <tbremer19@gmail.com>
2014-12-30 16:34:09 +0100

    Manifest changes, Changed theme

--- app/src/main/java/supremez2/zwskin/diamondinc/com/supremezdashboard/MainActivity.java
+++ app/src/main/java/supremez2/zwskin/diamondinc/com/supremezdashboard/MainActivity.java
@@ -2,6 +2,7 @@ package supremez2.zwskin.diamondinc.com.supremezdashboard;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getTitle();
     }
 
 
