8cad616f2937eb22b7a80d99eadae7d491c548bd
arturomb21 <arthuralhcrew@gmail.com>
2014-12-31 03:04:51 +0100

    Added Color

--- app/src/main/java/supremez2/zwskin/diamondinc/com/supremezdashboard/MainActivity.java
+++ app/src/main/java/supremez2/zwskin/diamondinc/com/supremezdashboard/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.graphics.drawable.Drawable;
+
 
 
 public class MainActivity extends ActionBarActivity {
