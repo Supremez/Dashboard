c3fa8876c1544b8232ab852e274f699c62a98eca
Tim Bremer <tbremer19@gmail.com>
2015-01-02 19:55:24 +0100

    Added FAB

--- app/src/main/java/supremez2/zwskin/diamondinc/com/supremezdashboard/BaseActivity.java
+++ app/src/main/java/supremez2/zwskin/diamondinc/com/supremezdashboard/BaseActivity.java
@@ -8,16 +8,21 @@ import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AdapterView;
 import android.support.v7.widget.CardView;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.melnykov.fab.FloatingActionButton;
+import com.melnykov.fab.ObservableScrollView;
 import com.nineoldandroids.animation.Animator;
+
+import java.util.Observable;
 
 
 public abstract class BaseActivity extends ActionBarActivity {
 
     public Toolbar toolbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +38,25 @@ public abstract class BaseActivity extends ActionBarActivity {
 
 
         }
+
+
+        ScrollView scrollView = (ScrollView) findViewById(R.id.scroll);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        //TODO Attatch FAB to ScrollView
+        //fab.attachToScrollView(scrollView);
+       fab.setOnClickListener(new View.OnClickListener() {
+
+           public void onClick(View v) {
+               //TODO Add click action
+
+           }
+       });
+
     }
 
     public void wallpaper(View view) {
-        Intent intent = new Intent(this, com.antonioleiva.materialeverywhere.HomeActivity.class);
+      Intent intent = new Intent(this, com.antonioleiva.materialeverywhere.HomeActivity.class);
         startActivity(intent);
     }
 
