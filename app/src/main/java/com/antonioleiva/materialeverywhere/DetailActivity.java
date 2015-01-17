7efc570016499823d22903625ece52f8d1d8cde9
Tim Bremer <tbremer19@gmail.com>
2015-01-17 20:08:25 +0100

    Cropping works!

--- app/src/main/java/com/antonioleiva/materialeverywhere/DetailActivity.java
+++ app/src/main/java/com/antonioleiva/materialeverywhere/DetailActivity.java
@@ -3,10 +3,14 @@
 package com.antonioleiva.materialeverywhere;
 
 import android.app.WallpaperManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.view.ViewCompat;
@@ -17,8 +21,11 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.antonioleiva.materialeverywhere.uk.co.senab.photoview.PhotoViewAttacher;
+import com.soundcloud.android.crop.Crop;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 import supremez2.zwskin.diamondinc.com.supremezdashboard.R;
@@ -66,27 +73,70 @@ public class DetailActivity extends BaseActivity {
             return true;
         }
         if (id == R.id.set_wall) {
+
             mImageView = (ImageView) findViewById(R.id.image);
             Bitmap mBitmap = ((BitmapDrawable)mImageView.getDrawable()).getBitmap();
-            WallpaperManager myWallpaperManager = WallpaperManager
-                    .getInstance(getApplicationContext());
 
-            try {
-                myWallpaperManager.setBitmap(mBitmap);
-                Toast.makeText(DetailActivity.this, "Wallpaper set",
-                        Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                Toast.makeText(DetailActivity.this,
-                        "Error setting wallpaper", Toast.LENGTH_SHORT)
-                        .show();
-            }
-            return true;
+            Uri tempUri = getImageUri(getApplicationContext(), mBitmap);
+
+
+
+            beginCrop(tempUri);
+
 
 
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK) {
+            beginCrop(result.getData());
+        } else if (requestCode == Crop.REQUEST_CROP) {
+            handleCrop(resultCode, result);
+        }
+    }
+
+    private void beginCrop(Uri source) {
+        Uri outputUri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        new Crop(source).output(outputUri).start(this);
+    }
+
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+
+            mImageView.setImageURI(Crop.getOutput(result));
+
+            WallpaperManager myWallpaperManager = WallpaperManager
+                    .getInstance(getApplicationContext());
+
+            try {
+
+                Bitmap mBitmap = ((BitmapDrawable)mImageView.getDrawable()).getBitmap();
+                myWallpaperManager.setBitmap(mBitmap);
+                Toast.makeText(DetailActivity.this, "Wallpaper set",
+                        Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                Toast.makeText(DetailActivity.this,
+                        "Error setting wallpaper", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+        } else if (resultCode == Crop.RESULT_ERROR) {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     public static void launch(BaseActivity activity, View transitionView, String url) {
         ActivityOptionsCompat options =
